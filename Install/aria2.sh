882675c506c2d3db11fbc5da6962349e0be6f7e0
loosink <30341914+loosink@users.noreply.github.com>
2023-09-17 21:54:41 +0800

    Add files via upload

---
+++ Install/aria2.sh
@@ -0,0 +1,140 @@
+#/bin/bash
+sudo rm -rf ./install.sh
+cd /
+
+#Aria2密码
+p=1234
+
+#是否安装Apache2
+a=y
+
+#log,安装日志，保存于/root/log_of_install_aria2dash.log
+log="/root/log_of_install_aria2dash.log"
+date > $log
+
+#默认网页路径
+dir="/var/www/html"
+d=$dir
+
+if [ $d != $dir ] ; then
+    dir=$d
+    echo !
+fi
+
+
+while getopts ":p:a:d:h:" opt
+do
+    case $opt in
+        p)
+        p=$OPTARG
+        ;;
+        a)
+        a=$OPTARG
+        ;;
+        d)
+        d=$OPTARG
+        ;;
+        h)
+        h=$OPTARG
+        ;;
+        ?)
+        echo "WTF???"
+        exit 
+        ;;
+    esac
+done
+
+
+
+#判断系统是debian，Ubuntu，Fedora，cent还是手机的turmux
+
+if [[  $(command -v sudo apt)  ]] ; then
+        cmd="sudo apt"
+	echo "your system is Ubuntu/Debian" >>$log
+	apache2="apache2"
+    apt install sudo -y  #保证Debian能够使用sudo正常运行改脚本
+    apt install wget -y  #下载wget
+    apt install sudo -y  #保证sudo指令正常运行
+else
+        cmd="sudo yum"
+	
+	yum install epel-release -y
+	yum install aria2 -y
+ 	yum install wget -y
+	apache2="httpd"
+        firewall-cmd --zone=public --add-port=80/tcp --permanent  #centos防火墙规则添加
+	
+
+fi
+
+
+#安装必要的包
+$cmd update 
+#根据需要，安装Apache2
+if [ $a = "y" ] ; then
+    cmd1="$cmd install $apache2 -y"
+    $cmd1
+    sudo mv $dir/index.html $dir/index.html0
+else  
+    echo "I will not install apache2."
+fi
+
+#安装必要的
+cmd2="$cmd install screen vim aria2 unzip git curl -y"
+$cmd2
+
+# 下载AriaNg
+
+tmp="/tmp/Onekey-deploy_aria2"
+sudo rm -rf $tmp
+sudo git clone https://ghproxy.com/https://github.com/loosink/OneDriveUploader-Modify.git $tmp
+sudo rm -rf $dir/lixian
+sudo rm -rf $dir/downloads
+sudo mkdir -p $dir/lixian 
+sudo mkdir -p $dir/downloads
+sudo unzip $tmp/*.zip -d $dir/lixian
+sudo chmod 777 -R $dir/lixian
+link="<a href="http://$ip:8080" target="blank">"
+cat $dir/lixian/head.html > $dir/lixian/index.html
+echo $link >> $dir/lixian/index.html
+cat $dir/lixian/foot.html >> $dir/lixian/index.html
+#sudo rm -rf $dir/index.html
+
+#沃日，为啥一直bug。。。
+ip=$(curl -s https://ipinfo.io/ip)
+dir="/var/www/html"
+link="<a href="http://$ip:8080" target="blank">"
+sudo cat $dir/lixian/head.html > $dir/lixian/index.html
+sudo echo $link >> $dir/lixian/index.html
+sudo cat $dir/lixian/foot.html >> $dir/lixian/index.html
+#安装FileBrowser
+curl -fsSL https://ghproxy.com/raw.githubusercontent.com/loosink/OneDriveUploader-Modify/main/Install/getFileBrowser.sh | bash
+
+
+
+
+#开始配置aria2
+sudo rm -rf /root/.aria2
+sudo mkdir -p /root/.aria2
+sudo rm -rf /root/.aria2/*
+sudo touch /root/.aria2/aria2.session
+sudo mv /tmp/Onekey-deploy_aria2/aria2.conf /root/.aria2/
+sudo mv /tmp/Onekey-deploy_aria2/updatetracker.sh /root/.aria2/
+sudo rm -rf ./install.sh
+clear
+secret="rpc-secret=$p"
+upload="on-download-complete=/root/aria2upload.sh"
+sudo echo $upload >> /root/.aria2/aria2.conf
+
+#设置systemctl
+sudo cp $tmp/aria2c /etc/init.d/
+sudo cp $tmp/filebrowser /etc/init.d/
+sudo chmod 777  /etc/init.d/aria2c
+sudo chmod 777  /etc/init.d/filebrowser
+sudo systemctl daemon-reload
+sudo systemctl restart aria2c
+sudo systemctl restart filebrowser
+
+#设置开机自启
+sudo systemctl enable aria2c
+sudo systemctl enable filebrowser
---
+++ Install/getOneDriveUploader.sh
@@ -0,0 +1,106 @@
+Download_OneDriveUploader()
+{
+	trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; trap ERR; return 1' ERR
+	OneDriveUploader_os="unsupported"
+	OneDriveUploader_arch="unknown"
+	install_path="/usr/local/bin"
+
+	# Termux on Android has $PREFIX set which already ends with /usr
+	if [[ -n "$ANDROID_ROOT" && -n "$PREFIX" ]]; then
+		install_path="$PREFIX/bin"
+	fi
+
+	# Fall back to /usr/bin if necessary
+	if [[ ! -d $install_path ]]; then
+		install_path="/usr/bin"
+	fi
+
+	# Not every platform has or needs sudo (https://termux.com/linux.html)
+	((EUID)) && [[ -z "$ANDROID_ROOT" ]] && sudo_cmd="sudo"
+
+	#########################
+	# Which OS and version? #
+	#########################
+
+	OneDriveUploader_bin="OneDriveUploader"
+	OneDriveUploader_dl_ext="zip"
+
+	# NOTE: `uname -m` is more accurate and universal than `arch`
+	# See https://en.wikipedia.org/wiki/Uname
+	unamem="$(uname -m)"
+	case $unamem in
+	*aarch64*)
+		OneDriveUploader_arch="arm";;
+	*64*)
+		OneDriveUploader_arch="amd64";;
+	*86*)
+		OneDriveUploader_arch="i386";;
+	*arm*)
+		OneDriveUploader_arch="arm";;
+	*)
+		echo "Aborted, unsupported or unknown architecture: $unamem"
+		return 2
+		;;
+	esac
+
+	# NOTE: `uname -m` is more accurate and universal than `arch`
+	# See https://en.wikipedia.org/wiki/Uname
+	unameu="$(tr '[:lower:]' '[:upper:]' <<<$(uname))"
+	if [[ $unameu == *LINUX* ]]; then
+		OneDriveUploader_os="linux"
+	else
+		echo "Aborted, unsupported or unknown OS: $uname"
+		return 6
+	fi
+
+	########################
+	# Download and extract #
+	########################
+
+	echo "Downloading File Browser for $OneDriveUploader_os/$OneDriveUploader_arch..."
+	if type -p curl >/dev/null 2>&1; then
+		net_getter="curl -fsSL"
+	elif type -p wget >/dev/null 2>&1; then
+		net_getter="wget -qO-"
+	else
+		echo "Aborted, could not find curl or wget"
+		return 7
+	fi
+	
+	OneDriveUploader_file="OneDriveUploader_"$OneDriveUploader_arch"_"$OneDriveUploader_os".$OneDriveUploader_dl_ext"
+	OneDriveUploader_tag="$(${net_getter}  https://api.github.com/repos/loosink/OneDriveUploader-Modify/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"//g' | sed 's/tag_name: //g')"
+	OneDriveUploader_url="https://ghproxy.com/https://github.com/loosink/OneDriveUploader-Modify/releases/download/$OneDriveUploader_tag/$OneDriveUploader_file"
+	echo "$OneDriveUploader_url"
+
+	# Use $PREFIX for compatibility with Termux on Android
+	rm -rf "$PREFIX/tmp/$OneDriveUploader_file"
+
+	${net_getter} "$OneDriveUploader_url" > "$PREFIX/tmp/$OneDriveUploader_file"
+
+	echo "Extracting..."
+	case "$OneDriveUploader_file" in
+		*.zip)    unzip -o "$PREFIX/tmp/$OneDriveUploader_file" "$OneDriveUploader_bin" -d "$PREFIX/tmp/" ;;
+		*.tar.gz) tar -xzf "$PREFIX/tmp/$OneDriveUploader_file" -C "$PREFIX/tmp/" "$OneDriveUploader_bin" ;;
+	esac
+	chmod +x "$PREFIX/tmp/$OneDriveUploader_bin"
+
+	echo "Putting OneDriveUploader in $install_path (may require password)"
+	$sudo_cmd mv "$PREFIX/tmp/$OneDriveUploader_bin" "$install_path/$OneDriveUploader_bin"
+	if setcap_cmd=$(PATH+=$PATH:/sbin type -p setcap); then
+		$sudo_cmd $setcap_cmd cap_net_bind_service=+ep "$install_path/$OneDriveUploader_bin"
+	fi
+	$sudo_cmd rm -- "$PREFIX/tmp/$OneDriveUploader_file"
+
+	if type -p $OneDriveUploader_bin >/dev/null 2>&1; then
+		echo "Successfully installed"
+		trap ERR
+		return 0
+	else
+		echo "Something went wrong, OneDrive Uploader is not in your path"
+		trap ERR
+		return 1
+	fi
+
+}
+
+Download_OneDriveUploader
