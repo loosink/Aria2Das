fc662457aa3692ef331a272e8e2efa332595bf9e
loosink <30341914+loosink@users.noreply.github.com>
2023-09-17 21:15:35 +0800

    Update updatetracker.sh

--- updatetracker.sh
+++ updatetracker.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+list=`wget -qO- https://gitea.com/XIU2/TrackersListCollection/raw/branch/master/all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
     echo add......
