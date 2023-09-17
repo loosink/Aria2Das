f484e06291e242770070edcd8e85c1b875a4499f
loosink <30341914+loosink@users.noreply.github.com>
2023-09-18 01:45:02 +0800

    Update Aria2Dash.sh

--- Aria2Dash.sh
+++ Aria2Dash.sh
@@ -7,12 +7,16 @@ echo "the latest script is $script_url"
 
 echo "判断系统是debian，Ubuntu，Fedora，cent"
 
-if [[  $(command -v sudo apt)  ]] ; then
+if command -v apt >/dev/null 2>&1; then
         cmd="sudo apt"
 	echo "your system is Ubuntu/Debian"
 	apache2="apache2"
-else
+elif command -v yum >/dev/null 2>&1; then
         cmd="sudo yum"
+else
+    # 如果系统中没有yum或apt命令
+    echo "无法识别Linux包管理器"
+    exit 1
 fi
 
 echo "使用$cmd进行curl的安装"
