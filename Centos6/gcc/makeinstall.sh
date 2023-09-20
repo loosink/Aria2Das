3e5704c7d627e7defaa6b97af4b3898a11b57f43
loosink <30341914+loosink@users.noreply.github.com>
2023-09-20 13:50:51 +0800

    Update makeinstall.sh

--- Centos6/gcc/makeinstall.sh
+++ Centos6/gcc/makeinstall.sh
@@ -11,25 +11,6 @@ tar -jxf mpfr-2.4.2.tar.bz2
 tar -jxf gmp-4.3.2.tar.bz2
 tar -zxvf mpc-0.8.1.tar.gz
 
-#编译安装gcc4.8.5
-wget https://ghproxy.com/https://github.com/loosink/Aria2Das/releases/download/2.2.2/gcc-4.8.5.tar.bz2
-tar -jxf gcc-4.8.5.tar.bz2
-cd gcc-4.8.5
-chmod +x *
-./contrib/download_prerequisites
-mkdir gcc-build-4.8.5
-cd gcc-build-4.8.5
-../configure -enable-checking=release -enable-languages=c,c++ -disable-multilib
-yum install -y glibc-devel.i686 glibc-devel
-make
-make install
-cd /root
-rm -rf gcc*
-
-#备份旧版gcc
-#mv /usr/bin/gcc /usr/bin/gcc-4.4.7
-#mv /usr/bin/g++ /usr/bin/g++-4.4.7
-#echo 'export PATH=$PATH:/usr/local/gcc-4.8.5/bin/' | sudo tee -a /etc/profile
 
 #编译安装gmp
 cd gmp-4.3.2
@@ -66,6 +47,28 @@ rm -rf /bin/gettext /usr/bin/gettext /usr/bin/gettext.sh /usr/lib64/gettext
 gettext -V
 cd /root
 rm -rf gettext*
+
+#编译安装gcc4.8.5
+wget https://ghproxy.com/https://github.com/loosink/Aria2Das/releases/download/2.2.2/gcc-4.8.5.tar.bz2
+tar -jxf gcc-4.8.5.tar.bz2
+cd gcc-4.8.5
+chmod +x *
+./contrib/download_prerequisites
+mkdir gcc-build-4.8.5
+cd gcc-build-4.8.5
+../configure -enable-checking=release -enable-languages=c,c++ -disable-multilib
+yum install -y glibc-devel.i686 glibc-devel
+make
+make install
+cd /root
+rm -rf gcc*
+
+#备份旧版gcc
+#mv /usr/bin/gcc /usr/bin/gcc-4.4.7
+#mv /usr/bin/g++ /usr/bin/g++-4.4.7
+#echo 'export PATH=$PATH:/usr/local/gcc-4.8.5/bin/' | sudo tee -a /etc/profile
+
+
 
 wget https://ghproxy.com/https://github.com/loosink/Aria2Das/blob/master/Centos6/gcc/autoconf-2.71.zip
 unzip autoconf-2.71.zip
