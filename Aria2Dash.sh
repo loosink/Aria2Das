05e02d9fd6558016b8b17b962e43bf3ce229ba90
loosink <30341914+loosink@users.noreply.github.com>
2023-09-21 10:44:33 +0800

    Update Aria2Dash.sh

--- Aria2Dash.sh
+++ Aria2Dash.sh
@@ -2,7 +2,7 @@
 #get the latest Aria2Dash
 #用来获取Aria2Dash的最新版本，如果是fork我脚本的，请自行更改tag_name和script_url的链接。
 tag_name="$(curl -s https://api.github.com/repos/loosink/Aria2Das/releases/latest | grep -o '"tag_name": ".*"' | sed 's/"//g' | sed 's/tag_name: //g')"
-script_url="https://ghproxy.com/https://github.com/loosink/Aria2Das/releases/download/$tag_name/install.sh"
+script_url="https://github.com/loosink/Aria2Das/releases/download/$tag_name/install.sh"
 echo "the latest script is $script_url"
 
 echo "判断系统是debian，Ubuntu，Fedora，cent"
