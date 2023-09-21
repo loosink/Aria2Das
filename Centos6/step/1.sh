c6726ee6467d2d234654b415e48a783d4cefc27f
loosink <30341914+loosink@users.noreply.github.com>
2023-09-21 13:06:37 +0800

    Update 1.sh

--- Centos6/step/1.sh
+++ Centos6/step/1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf /root/Aria2Das/
+
 #定义yum安装命令
 yum install -y wget curl unzip
 yum install -y gcc
@@ -9,6 +9,5 @@ yum install -y gcc-c++
 yum install -y git
 yum install dos2unix -y
 
-cd "/root"
 #git clone https://ghproxy.com/https://github.com/loosink/Aria2Das.git
 echo "1.sh运行完毕"
